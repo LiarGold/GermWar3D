9119c09bf1501c51865a09725a030f96b9d8cea3
MSDN-SPECIAL\Administrator <mastergold@naver.com>
2019-01-22 02:30:15 +0900

    스카이박스 및 지구 생성, 메테오 생성

--- Assets/Script/YouShallNotPass/GameScene/Object/CMeteor.cs
+++ Assets/Script/YouShallNotPass/GameScene/Object/CMeteor.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class CMeteor : MonoBehaviour
 {
-	//! 초기화
-	public void Start()
+	public ParticleSystem _impactParticle = null;
+
+	//! 피탄되었을 경우
+	public void OnCollisionEnter(Collision collision)
 	{
-		
+		var impactPos = collision.contacts[0].point;
+		var impactDir = impactPos - transform.position;
+
+		_impactParticle.transform.position = impactPos;
+		_impactParticle.transform.Rotate(-impactDir);
+		_impactParticle.Play();
+
+		Destroy(collision.gameObject, 1.0f);
 	}
 }
